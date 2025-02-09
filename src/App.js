55d8d66c0972975a9a0f19d628ee8f68acc7654f
Sandeep Garg <testanalyst@live.com>
2025-02-09 12:32:23 +0530

    Initial React hello World Setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+// src/App.js
+import React from 'react';
+
+const App = () => {
+  return <h1>Hello World!</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+// webpack.config.js
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  devServer: {
+    static: './dist'
+  }
+};
